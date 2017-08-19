6d6ae846c62a5a887b9e579a830d936f3304381d
0gravity000 <0gravity000@gmail.com>
2017-08-19 04:22:14 +0000

    Debuged SessionsHelperTest error

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,10 +30,12 @@ module SessionsHelper
     end
   end
   
+=begin
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+=end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
